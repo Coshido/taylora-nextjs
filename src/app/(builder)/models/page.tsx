c9de9e90293af187d2fa0da0ceec43d187c1f505
Coshido <coshido@gmail.com>
2023-06-20 14:04:44 +0200

    dev: model conditional style

--- src/app/(builder)/models/page.tsx
+++ src/app/(builder)/models/page.tsx
@@ -1,22 +1,29 @@
 "use client";
 
+import Radio from "@/src/components/Radio";
 import { useProduct } from "@/src/context/ProductContext";
 import { fetchAll } from "@/src/lib/fetchAll";
 import Image from "next/image";
+import { useState } from "react";
 
 type Props = {};
 
 const Models = (props: Props) => {
   const { model, setModel } = useProduct();
-
   const data = fetchAll();
+
+  const onClickHandler = (model: string) => {
+    setModel(model);
+  };
 
   const content = data.map((ele) => {
     return (
       <div
         key={ele.id}
-        className="p-11 gap-5 border-2 rounded-sm border-[#ededed] flex flex-col hover:cursor-pointer active:border-[#ffb500]"
-        onClick={() => setModel(ele.model)}
+        className={`p-11 gap-5 border-2 rounded-sm  flex flex-col hover:cursor-pointer ${
+          ele.model === model ? "border-[#ffb500]" : "border-[#ededed]"
+        }`}
+        onClick={() => onClickHandler(ele.model)}
       >
         <h2 className="text-[2rem] font-bold font-lato text-text-grey">
           BMW {ele.model}
@@ -29,9 +36,14 @@ const Models = (props: Props) => {
           sizes="100vw"
           className="w-auto h-auto"
         />
-        <p className="font-lato text-t-lightgrey ">
-          from {Intl.NumberFormat("en-US").format(ele.basePrice)}$
+        <p className="font-lato text-t-lightgrey focus:bg-inherit">
+          from ${Intl.NumberFormat("en-US").format(ele.basePrice)}
         </p>
+        {ele.model === model ? (
+          <Radio active={true} />
+        ) : (
+          <Radio active={false} />
+        )}
       </div>
     );
   });
---
+++ src/components/Radio.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  active: Boolean;
+};
+
+const Radio = (props: Props) => {
+  const content = props.active ? (
+    <div className=" bg-[#ffb500] h-[30px] w-[30px] self-center rounded-full">
+      <Image
+        src="/cd-icon-check.svg"
+        alt="Radio Button"
+        height="30"
+        width="30"
+      />
+    </div>
+  ) : (
+    <div className="h-[30px] w-[30px] self-center flex align-middle justify-center">
+      <div className="border-[#ededed] border-2 rounded-full h-[26px] w-[26px]" />
+    </div>
+  );
+
+  return content;
+};
+
+export default Radio;
